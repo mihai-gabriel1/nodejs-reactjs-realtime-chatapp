03ec5dc0a51f559b2f33d94053894f089a6745fd
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-16 22:03:00 +0300

    Chats page finished; we can now succesfully create a chat, join a chat, or even add other users to the chat room

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,23 @@
-const ChatsPage = () => {
-  return <>chats...</>;
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "ffec9488-dddb-4263-9755-0cb8d66ac18d",
+    props.user.username,
+    props.user.secret
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
 };
 
 export default ChatsPage;
+// ffec9488-dddb-4263-9755-0cb8d66ac18d
